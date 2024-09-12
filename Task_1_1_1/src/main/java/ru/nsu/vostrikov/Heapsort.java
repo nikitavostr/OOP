108ea654119f75171f69db9b1b5a4d15a905a2d3
Nikita Vostrikov <n.vostrikov@g.nsu.ru>
2024-09-12 10:14:54 +0700

    Task_1_1_1

--- Task_1_1_1/src/main/java/ru/nsu/vostrikov/Heapsort.java
+++ Task_1_1_1/src/main/java/ru/nsu/vostrikov/Heapsort.java
@@ -11,23 +11,23 @@ import java.util.Random;
 public class Heapsort {
     /**
      * this function build a heap.
+     *
      * @param arr - array
-     * @param n - size of array
-     * @param i - index of current element
+     * @param n   - size of array
+     * @param i   - index of current element
      */
 
-    public static void buildHeap(int[] arr, int n, int i)
-    {
+    public static void buildHeap(int[] arr, int n, int i) {
         int dad = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
 
-        if (left < n && arr[left] > arr[dad])
+        if (left < n && arr[left] > arr[dad]) {
             dad = left; // if left child bigger than dad child becomes dad
-
-        if (right < n && arr[right] > arr[dad])
+        }
+        if (right < n && arr[right] > arr[dad]) {
             dad = right; // analogically
-
+        }
         if (dad != i) {
             int temp = arr[i];
             arr[i] = arr[dad];
@@ -39,13 +39,15 @@ public class Heapsort {
 
     /**
      * this is sorting function
+     *
      * @param arr - array
-     * @param n - size of array
+     * @param n   - size of array
      */
-    public static void heapSort(int[] arr, int n)
-    {
-        for (int i = n / 2 - 1; i >= 0; i--)
+
+    public static void heapSort(int[] arr, int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
             buildHeap(arr, n, i); // call a build_heap function for build max heap
+        }
         for (int i = n - 1; i > 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
@@ -54,10 +56,11 @@ public class Heapsort {
         }
     }
 
-    /** testing complexity
+    /**
+     * testing complexity
      */
     public static void main(String[] args) {
-        for(int i = 1000000; i <= 7000000; i += 1000000){
+        for (int i = 1000000; i <= 7000000; i += 1000000) {
             int[] arr = new int[i];
             Random rd = new Random();
             for (int j = 0; j < i; j++) {
--- Task_1_1_1/src/test/java/ru/nsu/vostrikov/HeapsortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/vostrikov/HeapsortTest.java
@@ -7,53 +7,50 @@ import org.junit.jupiter.api.Test;
 class HeapsortTest {
     @Test
     void OneNumTest() {
-        int[] testArray = new int[] {7};
-        Heapsort.heapSort(testArray , 1);
-        assertArrayEquals(new int[] {7}, testArray);
+        int[] testArray = new int[]{7};
+        Heapsort.heapSort(testArray, 1);
+        assertArrayEquals(new int[]{7}, testArray);
     }
 
     @Test
     void ZeroNumTest() {
-        int[] testArray = new int[] {};
-        Heapsort.heapSort(testArray , 0);
-        assertArrayEquals(new int[] {}, testArray);
+        int[] testArray = new int[]{};
+        Heapsort.heapSort(testArray, 0);
+        assertArrayEquals(new int[]{}, testArray);
     }
 
     @Test
     void NegativeNumTest() {
-        int[] testArray = new int[] {-1, -5, -3, -10, -34};
-        Heapsort.heapSort(testArray , 5);
-        assertArrayEquals(new int[] {-34, -10, -5, -3, -1}, testArray);
+        int[] testArray = new int[]{-1, -5, -3, -10, -34};
+        Heapsort.heapSort(testArray, 5);
+        assertArrayEquals(new int[]{-34, -10, -5, -3, -1}, testArray);
     }
 
     @Test
     void SortedArrTest() {
-        int[] testArray = new int[] {1, 2, 3, 4, 5};
-        Heapsort.heapSort(testArray , 5);
-        assertArrayEquals(new int[] {1, 2, 3, 4, 5}, testArray);
+        int[] testArray = new int[]{1, 2, 3, 4, 5};
+        Heapsort.heapSort(testArray, 5);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, testArray);
     }
 
     @Test
     void DescendingOrderTest() {
-        int[] testArray = new int[] {5, 4, 3, 2, 1};
-        Heapsort.heapSort(testArray , 5);
-        assertArrayEquals(new int[] {1, 2, 3, 4, 5}, testArray);
+        int[] testArray = new int[]{5, 4, 3, 2, 1};
+        Heapsort.heapSort(testArray, 5);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, testArray);
     }
 
     @Test
     void NegativeAndPositiveNumTest() {
-        int[] testArray = new int[] {-100, 54, 32, -154, 452};
-        Heapsort.heapSort(testArray , 5);
-        assertArrayEquals(new int[] {-154, -100, 32, 54, 452}, testArray);
+        int[] testArray = new int[]{-100, 54, 32, -154, 452};
+        Heapsort.heapSort(testArray, 5);
+        assertArrayEquals(new int[]{-154, -100, 32, 54, 452}, testArray);
     }
 
     @Test
     void SameNumTest() {
-        int[] testArray = new int[] {1, 4, 6, 3, 1};
-        Heapsort.heapSort(testArray , 5);
-        assertArrayEquals(new int[] {1, 1, 3, 4, 6}, testArray);
+        int[] testArray = new int[]{1, 4, 6, 3, 1};
+        Heapsort.heapSort(testArray, 5);
+        assertArrayEquals(new int[]{1, 1, 3, 4, 6}, testArray);
     }
-
-
-
 }
