3969443d2a40612e93e6331097c1bd203f885ddf
Nikita Vostrikov <n.vostrikov@g.nsu.ru>
2024-10-07 15:00:47 +0700

    done

--- Task_1_1_3/src/test/java/ru/nsu/vostrikov/ParseStrTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/vostrikov/ParseStrTest.java
@@ -1,7 +1,8 @@
 package ru.nsu.vostrikov;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class ParseStrTest {
     @Test
