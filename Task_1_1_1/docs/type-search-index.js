6dc3baf778049e5253694f5bf2d9055e29895863
Nikita Vostrikov Vladimirovich <n.vostrikov@g.nsu.ru>
2024-10-07 13:49:08 +0700

    Delete Task_1_1_1 directory

--- Task_1_1_1/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.vostrikov","l":"Heapsort"}];updateSearchResults();
--- Task_1_1_1/src/main/java/script.sh
+++
@@ -1,3 +0,0 @@
-javac ru/nsu/vostrikov/Heapsort.java
-javadoc ru/nsu/vostrikov/Heapsort.java
-java ru.nsu.vostrikov.Heapsort
