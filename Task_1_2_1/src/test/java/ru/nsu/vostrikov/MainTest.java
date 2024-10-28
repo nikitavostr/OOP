844f1d5ae64b19eb6ede519e1b4a82ca0eef633f
Nikita Vostrikov <n.vostrikov@g.nsu.ru>
2024-10-29 00:52:21 +0700

    Restore folder to prevent deletion in pull request

---
+++ Task_1_2_1/src/test/java/ru/nsu/vostrikov/MainTest.java
@@ -0,0 +1,14 @@
+package ru.nsu.vostrikov;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void test()  {
+        Main.main(null);
+        assertTrue(true);
+    }
+}
