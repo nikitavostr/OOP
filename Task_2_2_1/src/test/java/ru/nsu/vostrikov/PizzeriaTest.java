0a52a48f7d7c52fa5043f21785601629ee19fb14
Nikita Vostrikov <n.vostrikov@g.nsu.ru>
2025-03-13 19:12:51 +0700

    fix

--- Task_2_2_1/src/test/java/ru/nsu/vostrikov/PizzeriaTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/vostrikov/PizzeriaTest.java
@@ -2,6 +2,7 @@ package ru.nsu.vostrikov;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
