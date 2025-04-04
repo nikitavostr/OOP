21e1cfc64f6fe55945128a655b23e1bd8fd72a5d
Nikita Vostrikov <n.vostrikov@g.nsu.ru>
2025-04-04 15:14:03 +0700

    add tests

--- Task_2_3_1/src/test/java/ru/nsu/vostrikov/snake/GameModelTest.java
+++ Task_2_3_1/src/test/java/ru/nsu/vostrikov/snake/GameModelTest.java
@@ -1,9 +1,12 @@
 package ru.nsu.vostrikov.snake;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -38,7 +41,6 @@ class GameModelTest {
 
     @Test
     void testSnakeEatsFood() {
-        // Помещаем еду перед змеёй
         gameModel.getFood().clear();
         gameModel.getFood().add(new FoodModel(gameModel.getSnake().getHead().getRow(),
                 gameModel.getSnake().getHead().getCol() + 1));
@@ -59,4 +61,20 @@ class GameModelTest {
         assertTrue(gameModel.isLost());
     }
 
+    @Test
+    void testFoodDoesNotSpawnInsideSnake() {
+        Set<Position> snakeBody = new HashSet<>(gameModel.getSnake().getBody());
+        for (FoodModel food : gameModel.getFood()) {
+            assertFalse(snakeBody.contains(new Position(food.getRow(), food.getCol())));
+        }
+    }
+
+    @Test
+    void testSnakeCannotReverse() {
+        gameModel.setDirection(Direction.RIGHT);
+        gameModel.movement();
+        gameModel.setDirection(Direction.LEFT);
+        gameModel.movement();
+        assertEquals(Direction.RIGHT, gameModel.getDirection());
+    }
 }
